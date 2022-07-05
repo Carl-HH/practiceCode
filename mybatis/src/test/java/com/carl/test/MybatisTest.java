469c7bb9ffb495edbca1fe5c245bbca9401ff784
carl <hc602197835@gmail.com>
2022-07-05 23:01:20 +0800

    change dictionary

--- mybatis/src/test/java/com/carl/test/MybatisTest.java
+++ mybatis/src/test/java/com/carl/test/MybatisTest.java
@@ -30,10 +30,10 @@ public class MybatisTest {
         //6.释放资源
         session.close();
         in.close();
-        try {
-            Thread.sleep(10000000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(10000000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
